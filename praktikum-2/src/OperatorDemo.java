91b9d48c292e850e1dc11cca32475523869d8aef
aisyahandini <aisyahh11ran@gmail.com>
2026-09-08 10:45:35 +0700

    Operator

--- praktikum-2/src/OperatorDemo.java
+++ praktikum-2/src/OperatorDemo.java
@@ -6,6 +6,11 @@ public class OperatorDemo {
         System.out.println("Kali: " + (a * b));
         System.out.println("Bagi: " + (a / b));
         System.out.println("Sisa Bagi: " + (a % b));
-        
+        /* lajut */
+        int nilaiUjian = 80;
+        boolean hadir = true;
+        System.out.println("Lulus (>=75): " + (nilaiUjian >= 75));
+        System.out.println("lulus DAN hadir: " + (nilaiUjian >=75 && hadir));
+
     }
 }
