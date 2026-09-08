169378080d950b3b6c7b9d1c261aaabe4ab72015
aisyahandini <aisyahh11ran@gmail.com>
2026-09-08 11:20:58 +0700

    BiodataSaya

---
+++ praktikum-2/src/BiodataSaya.java
@@ -0,0 +1,7 @@
+public class BiodataSaya {
+    public static void main (String[] args ){
+        // menampilkan nama dan nim dalam satu baris
+        System.out.println("Nama: AISYAH ANDINI ");
+        System.out.println("NIM: 2025573010001");
+    }
+}
