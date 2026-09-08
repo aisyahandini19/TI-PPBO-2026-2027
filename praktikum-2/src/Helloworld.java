1e08669e05de33b3f23df00ff2734b75e8c7a38a
aisyahandini <aisyahh11ran@gmail.com>
2026-09-08 09:47:08 +0700

    Sturktur Dasar Program Java

--- praktikum-2/src/Helloworld.java
+++ praktikum-2/src/Helloworld.java
@@ -1,15 +1,7 @@
 public class Helloworld {
     public static void main(String[] args){
-        System.out.println("AISYAH ANDINI ");
-        System.out.print("Nama: ");
-        System.out.print("Budi");
-        System.out.println();
-        System.out.println("Ini baris baru");
+        System.out.println("Halo, !Dunia ");
 
-        // Program Menampilkan Biodata sederhan
-        System.out.println("Nama: Aisyah Andini");
-        /*baris di bawah ini mencatak NIM mahasiswa*/
-        System.out.println("NIM: 2025573010001");
 
     }
 }
