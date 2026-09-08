aff595c75898f254023c08a4a9009f038f7adce9
aisyahandini <aisyahh11ran@gmail.com>
2026-09-08 10:00:45 +0700

    Stuktur Dasar Program Java

--- praktikum-2/src/Helloworld.java
+++ praktikum-2/src/Helloworld.java
@@ -1,7 +1,11 @@
 public class Helloworld {
     public static void main(String[] args){
         System.out.println("Halo, !Dunia ");
-
+        /*A.2*/
+        System.out.print("Nama: ");
+        System.out.print("AISYAH ANDINI");
+        System.out.println();
+        System.out.println("Ini baris baru");
 
     }
 }
