4e2cdffdc315ae11b23b6abc04032261a555c323
aisyahandini <aisyahh11ran@gmail.com>
2026-09-08 10:03:02 +0700

    Komentar Kode

--- praktikum-2/src/Helloworld.java
+++ praktikum-2/src/Helloworld.java
@@ -6,6 +6,11 @@ public class Helloworld {
         System.out.print("AISYAH ANDINI");
         System.out.println();
         System.out.println("Ini baris baru");
+        /*A.3*/
+        // Program Menampilkan Biodata sederhan
+        System.out.println("Nama: AISYAH ANDINI");
+        /*baris di bawah ini mencatak NIM mahasiswa*/
+        System.out.println("NIM: 2025573010001");
 
     }
 }
