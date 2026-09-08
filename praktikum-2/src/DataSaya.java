134e8145fa98099031918741dcd916d7a1e49938
aisyahandini <aisyahh11ran@gmail.com>
2026-09-08 11:34:24 +0700

    DataSaya soal ke 3

---
+++ praktikum-2/src/DataSaya.java
@@ -0,0 +1,15 @@
+public class DataSaya {
+    public static void main(String[] args){
+        String nama = "AISYAH ANDINI";
+        int umur = 19;
+        double tinggi = 154;
+        char inisial = 'p';
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Inisial: " + inisial);
+        System.out.println("Aktif: " + mahasiswaAktif);
+    }
+}
